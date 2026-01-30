6281080ddaea14003cd51b6666fc1bbab56c826e
anuj2110 <atrehan789@gmail.com>
2026-01-30 20:28:53 +0530

    added code for syncpractice

---
+++ 13_syncpractice/counter_test.go
@@ -0,0 +1,41 @@
+package syncpractice
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("Incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := Counter{}
+
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+		assertCounter(t, counter, 3)
+	})
+
+	t.Run("It runs safe concurrently", func(t *testing.T) {
+		wanted := 1000
+		counter := Counter{}
+		var wg sync.WaitGroup
+		wg.Add(wanted)
+
+		for _ = range wanted {
+			go func () {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		assertCounter(t, counter, wanted)
+
+	})
+}
+
+func assertCounter(t testing.TB, got Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}
